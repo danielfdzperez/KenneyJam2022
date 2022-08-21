424d41598892c63c8f097ecb570c3c2c80cbe6f4
JuanmaAA <08.dark.axel.08@gmail.com>
2022-08-21 12:36:12 +0200

    Menu boton start
    Ahora el menú start lleva al juego

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 var menu_origin_position := Vector2.ZERO
 var menu_origin_size := Vector2.ZERO
+var menu_transition_time := 0.5
 
 var current_menu
 var menu_stack := []
@@ -9,6 +10,7 @@ var menu_stack := []
 
 onready var menu_0  = $Menu0
 onready var menu_1  = $Menu1
+onready var tween = $Tween
 
 
 
@@ -16,15 +18,21 @@ func _ready() -> void:
 	menu_origin_position = Vector2(0,0)
 	menu_origin_size =  get_viewport().size
 	current_menu = menu_0
-	print("vamoh a ve---------" , menu_0.rect_global_position)
+	print("vamoh a ve---------" , tween)
 	print("X del vector    ", -menu_origin_size.x)
 	
 func move_to_next_menu(next_menu_id: String):
 	var next_menu = get_menu_from_menu_id(next_menu_id)
 	current_menu.rect_global_position = Vector2(-menu_origin_size.x, 0)
 	next_menu.rect_global_position = menu_origin_position
+	tween.interpolate_property(current_menu, "rect_global_position", current_menu.rect_global_position, 
+	Vector2(-menu_origin_size.x, 0), menu_transition_time,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT,0)
+	tween.interpolate_property(next_menu, "rect_global_position", next_menu.rect_global_position, 
+	menu_origin_position, menu_transition_time,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT,0)
+	tween.start()
 	menu_stack.append(current_menu)
 	current_menu = next_menu
+	
 	
 func move_to_previous_menu():
 	var previous_menu = menu_stack.pop_back()
@@ -52,7 +60,7 @@ func _on_Back_pressed():
 
 
 func _on_Start_pressed():
-	pass # Replace with function body.
+	Global.goto_scene("res://Main.tscn")
 
 
 func _on_Credits_pressed():
